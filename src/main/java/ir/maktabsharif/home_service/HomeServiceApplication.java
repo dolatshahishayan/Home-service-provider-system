a7f76bcfc29f8a930921eed1b308afd8d5224973
Shayan <dowlatshahis1382@gmail.com>
2025-06-15 20:30:18 +0330

    added persistence file and entity manager bean

--- src/main/java/ir/maktabsharif/home_service/HomeServiceApplication.java
+++ src/main/java/ir/maktabsharif/home_service/HomeServiceApplication.java
@@ -1,7 +1,10 @@
 package ir.maktabsharif.home_service;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomeServiceApplication {
@@ -9,5 +12,10 @@ public class HomeServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(HomeServiceApplication.class, args);
     }
-
+    @Bean
+    public EntityManager entityManager() {
+        try (EntityManager em = Persistence.createEntityManagerFactory("default").createEntityManager()) {
+            return em;
+        }
+    }
 }
