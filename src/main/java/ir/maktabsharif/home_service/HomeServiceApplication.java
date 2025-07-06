a6b1a183ae0f017652da21d028318a266de590ad
Shayan <dowlatshahis1382@gmail.com>
2025-07-06 15:55:01 +0330

    removed some warnings

--- src/main/java/ir/maktabsharif/home_service/HomeServiceApplication.java
+++ src/main/java/ir/maktabsharif/home_service/HomeServiceApplication.java
@@ -1,10 +1,7 @@
 package ir.maktabsharif.home_service;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.Persistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomeServiceApplication {
