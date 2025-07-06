e32daa1428cc6eea7c9f394df3978b58275d2058
Shayan <dowlatshahis1382@gmail.com>
2025-07-06 18:22:06 +0330

    removed some warnings

--- src/main/java/ir/maktabsharif/home_service/HomeServiceApplication.java
+++ src/main/java/ir/maktabsharif/home_service/HomeServiceApplication.java
@@ -2,7 +2,6 @@ package ir.maktabsharif.home_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HomeServiceApplication {
