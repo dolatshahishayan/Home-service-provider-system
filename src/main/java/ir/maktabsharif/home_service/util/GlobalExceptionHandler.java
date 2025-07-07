2479e2a970e237be24b57e8857e7bd2728949592
Shayan <dowlatshahis1382@gmail.com>
2025-07-07 16:37:43 +0330

    fix: replaced controller advice annotation with rest controller advice

--- src/main/java/ir/maktabsharif/home_service/util/GlobalExceptionHandler.java
+++ src/main/java/ir/maktabsharif/home_service/util/GlobalExceptionHandler.java
@@ -4,13 +4,13 @@ import ir.maktabsharif.home_service.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CouldNotUpdateException.class)
