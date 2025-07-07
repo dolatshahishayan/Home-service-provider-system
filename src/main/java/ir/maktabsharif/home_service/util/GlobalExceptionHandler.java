0aa60e0f76a6c8f8b1fb9d46a9f403cb201dcd68
Shayan <dowlatshahis1382@gmail.com>
2025-07-07 15:41:32 +0330

    feat: add exception handler for dto validation errors

--- src/main/java/ir/maktabsharif/home_service/util/GlobalExceptionHandler.java
+++ src/main/java/ir/maktabsharif/home_service/util/GlobalExceptionHandler.java
@@ -3,8 +3,12 @@ package ir.maktabsharif.home_service.util;
 import ir.maktabsharif.home_service.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -62,6 +66,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UserWithSameEmailExistsException.class)
     public ResponseEntity<String> handleUserWithSameEmailExistsException(UserWithSameEmailExistsException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
