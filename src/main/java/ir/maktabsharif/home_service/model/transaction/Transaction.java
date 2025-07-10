b39b9ab8c0e9ba588ed133af294b83dd16353656
Shayan <dowlatshahis1382@gmail.com>
2025-07-10 09:00:12 +0330

    fix: removed some warnings

--- src/main/java/ir/maktabsharif/home_service/model/transaction/Transaction.java
+++ src/main/java/ir/maktabsharif/home_service/model/transaction/Transaction.java
@@ -2,7 +2,6 @@ package ir.maktabsharif.home_service.model.transaction;
 
 import ir.maktabsharif.home_service.base.model.BaseEntity;
 import ir.maktabsharif.home_service.model.user.User;
-import ir.maktabsharif.home_service.model.wallet.Wallet;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
--- src/main/java/ir/maktabsharif/home_service/repository/order/OrderRepository.java
+++ src/main/java/ir/maktabsharif/home_service/repository/order/OrderRepository.java
@@ -3,7 +3,6 @@ package ir.maktabsharif.home_service.repository.order;
 import ir.maktabsharif.home_service.model.enums.OrderStatus;
 import ir.maktabsharif.home_service.model.order.Order;
 import ir.maktabsharif.home_service.model.user.Expert;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
